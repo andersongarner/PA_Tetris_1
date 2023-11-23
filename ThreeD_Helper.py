40224557d038962e8958d9038fbd326d6df9e4d2
andersongarner <69178397+andersongarner@users.noreply.github.com>
2023-11-23 10:58:35 -0500

    3D Display Done

--- ThreeD_Helper.py
+++ ThreeD_Helper.py
@@ -4,32 +4,27 @@ import copy
 
 
 def rotate_point_around(point_0, point_1, pitch, yaw, roll):
-    cosa = math.cos(roll)
-    sina = math.sin(roll)
-    cosb = math.cos(yaw)
-    sinb = math.sin(yaw)
-    cosc = math.cos(pitch)
-    sinc = math.sin(pitch)
-
+    cosa = math.cos(yaw)
+    sina = math.sin(yaw)
+    cosb = math.cos(pitch)
+    sinb = math.sin(pitch)
+    cosc = math.cos(roll)
+    sinc = math.sin(roll)
     axx = cosa * cosb
     axy = cosa * sinb * sinc - sina * cosc
     axz = cosa * sinb * cosc + sina * sinc
-
     ayx = sina * cosb
     ayy = sina * sinb * sinc + cosa * cosc
     ayz = sina * sinb * cosc - cosa * sinc
-
     azx = -1 * sinb
     azy = cosb * sinc
     azz = cosb * cosc
-
     p10x = point_1[0] - point_0[0]
     p10y = point_1[1] - point_0[1]
     p10z = point_1[2] - point_0[2]
-
     px = axx * p10x + axy * p10y + axz * p10z
     py = ayx * p10x + ayy * p10y + ayz * p10z
-    pz = azx * p10x * azy * p10y + azz * p10z
+    pz = azx * p10x + azy * p10y + azz * p10z
 
     return [px + point_0[0], py + point_0[1], pz + point_0[2]]
 
@@ -54,8 +49,8 @@ class Quad:
     def __init__(self, point_list: list, color):
         self.point_list = copy.deepcopy(self.point_list)
         self.color = copy.deepcopy(self.color)
-        self.point_list = point_list
-        self.color = color
+        self.point_list = copy.deepcopy(point_list)
+        self.color = copy.deepcopy(color)
 
     def move_all_points(self, xyz):
         for i in self.point_list:
@@ -85,7 +80,7 @@ def partition(array: list[Quad], low, high):
     pivot = array[high].get_avg(2)
     i = low - 1
     for j in range(low, high):
-        if array[j].get_avg(2) <= pivot:
+        if array[j].get_avg(2) >= pivot:
             i += 1
             (array[i], array[j]) = (array[j], array[i])
     (array[i+1], array[high]) = (array[high], array[i+1])
@@ -152,20 +147,31 @@ cam = uvage.Camera(1920, 1080)
 
 cube = Model()
 cube.position = [500, 500, 500]
-cube.add_quad(Quad([[100, 100, 100], [100, 100, -100], [100, -100, -100], [100, -100, 100]], [0, 0, 255]))
-cube.add_quad(Quad([[-100, 100, 100], [-100, 100, -100], [-100, -100, -100], [-100, -100, 100]], [255, 255, 0]))
-cube.add_quad(Quad([[100, 100, 100], [100, -100, 100], [-100, -100, 100], [-100, 100, 100]], [255, 0, 0]))
-cube.add_quad(Quad([[100, 100, -100], [100, -100, -100], [-100, -100, -100], [-100, 100, -100]], [0, 255, 0]))
-cube.add_quad(Quad([[100, 100, 100], [100, 100, -100], [-100, 100, -100], [-100, 100, 100]], [0, 255, 255]))
-cube.add_quad(Quad([[100, -100, 100], [100, -100, -100], [-100, -100, -100], [-100, -100, 100]], [255, 0, 255]))
+
+top_left_back = [-100, 100, -100]
+bottom_left_back = [-100, -100, -100]
+top_right_back = [100, 100, -100]
+bottom_right_back = [100, -100, -100]
+top_left_front = [-100, 100, 100]
+bottom_left_front = [-100, -100, 100]
+top_right_front = [100, 100, 100]
+bottom_right_front = [100, -100, 100]
+
+bottom_quad = Quad([bottom_right_front, bottom_right_back, bottom_left_back, bottom_left_front], [0, 0, 255])  # Blue
+top_quad = Quad([top_right_front, top_right_back, top_left_back, top_left_front], [0, 255, 255])  # Cyan
+left_quad = Quad([top_left_back, top_left_front, bottom_left_front, bottom_left_back], [255, 255, 0])  # Orange/Yellow
+right_quad = Quad([top_right_back, top_right_front, bottom_right_front, bottom_right_back], [0, 255, 0])  # Green
+back_quad = Quad([top_left_back, top_right_back, bottom_right_back, bottom_left_back], [255, 0, 255])  # Magenta
+front_quad = Quad([top_left_front, top_right_front, bottom_right_front, bottom_left_front], [255, 0, 0])  # Red
+
+cube.add_quad(bottom_quad)
+cube.add_quad(top_quad)
+cube.add_quad(left_quad)
+cube.add_quad(right_quad)
+cube.add_quad(back_quad)
+cube.add_quad(front_quad)
 
 my_cam = Camera()
-
-test_shape = Model()
-test_shape.position = [500, 500, 0]
-test_shape.add_quad(Quad([[50, 50, 0], [50, -50, 0], [-50, -50, 0], [-50, 50, 0]], [255, 0, 0]))
-test_shape.add_quad(Quad([[50, 50, 0], [50, -50, 0], [-50, -50, -50], [-50, 50, -50]], [255, 0, 255]))
-test_shape.rotate_degrees(45, 45, 45)
 
 current_shape = cube
 
@@ -173,12 +179,28 @@ current_shape = cube
 def tick():
     global current_shape
     cam.clear("black")
+    if uvage.is_pressing("left arrow"):
+        current_shape.position[0] -= 5
+    if uvage.is_pressing("right arrow"):
+        current_shape.position[0] += 5
+    if uvage.is_pressing("up arrow"):
+        current_shape.position[1] -= 5
+    if uvage.is_pressing("down arrow"):
+        current_shape.position[1] += 5
+    if uvage.is_pressing("i"):
+        current_shape.rotate_degrees(1, 0, 0)
+    if uvage.is_pressing("j"):
+        current_shape.rotate_degrees(0, 0, 1)
+    if uvage.is_pressing("k"):
+        current_shape.rotate_degrees(-1, 0, 0)
+    if uvage.is_pressing("l"):
+        current_shape.rotate_degrees(0, 0, -1)
     for i in current_shape.get_game_box_list(my_cam):
         cam.draw(i)
-    cube.rotate_degrees(1, 1, 1)
+    #cube.rotate_degrees(1, 1, 1)
     current_shape.draw_origin(cam)
     cam.display()
 
 
 
-uvage.timer_loop(30, tick)
+uvage.timer_loop(60, tick)
